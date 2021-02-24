dfd45a36e1db18b186e8d782f1d97edc84afd557
root <ahmed.ali.radwan94@gmail.com>
2021-02-24 23:15:36 +0000

    remove debug prints

--- scripts/autorally_gazebo_block.py
+++ scripts/autorally_gazebo_block.py
@@ -42,13 +42,13 @@ pub_chassisState = rospy.Publisher('/block/chassisState', chassisState, queue_si
 pub_wheelSpeeds = rospy.Publisher('/block/wheelSpeeds', wheelSpeeds, queue_size=10)
 pub_GPS = rospy.Publisher('/block/gpsRoverStatus', NavSatFix, queue_size=10)
 pub_IMU = rospy.Publisher('/block/imu/imu', Imu, queue_size=10)
-pub_gt = rospy.Publisher('/block/ground_truth/state', Odometry, queue_size=10)
+# pub_gt = rospy.Publisher('/block/ground_truth/state', Odometry, queue_size=10)
 
 pub_command = rospy.Publisher('/OCS/chassisCommand', chassisCommand, queue_size=10)
 
 
 def commandCallback(data_command):
-    print "got command"
+    # print "got command"
     pub_command.publish(data_command)
 
 def rightcamCallback(data_rightcam):
@@ -68,24 +68,24 @@ def rightcamCallback(data_rightcam):
 #     pub_rightcaminfo.publish(data_rightcaminfo)
 
 def chassisStateCallback(data_chassisState):
-    print "got chassis state"
+    # print "got chassis state"
     pub_chassisState.publish(data_chassisState)
 
 def wheelSpeedsCallback(data_wheelSpeeds):
-    print "got wheel speeds"
+    # print "got wheel speeds"
     pub_wheelSpeeds.publish(data_wheelSpeeds)
 
 def IMUCallback(data_IMU):
-    print "got IMU"
+    # print "got IMU"
     pub_IMU.publish(data_IMU)
 
 def GPSCallback(data_GPS):
-    print "got GPS"
+    # print "got GPS"
     pub_GPS.publish(data_GPS)
 
-def gtCallback(data_gt):
-    print "got ground truth"
-    pub_gt.publish(data_gt)
+# def gtCallback(data_gt):
+#     # print "got ground truth"
+#     pub_gt.publish(data_gt)
 
 rospy.Subscriber("/block/OCS/chassisCommand", chassisCommand, commandCallback)
 rospy.Subscriber("/right_camera/image_raw", Image, rightcamCallback)
@@ -96,6 +96,6 @@ rospy.Subscriber("/chassisState", chassisState, chassisStateCallback)
 rospy.Subscriber("/wheelSpeeds", wheelSpeeds, wheelSpeedsCallback)
 rospy.Subscriber("/gpsRoverStatus", NavSatFix, GPSCallback)
 rospy.Subscriber("/imu/imu", Imu, IMUCallback)
-rospy.Subscriber("/ground_truth/state", Odometry, gtCallback)
+# rospy.Subscriber("/ground_truth/state", Odometry, gtCallback)
 
 rospy.spin()
