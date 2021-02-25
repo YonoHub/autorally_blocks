f888a7bca822420c9d1c5fb0828e29a68f593137
root <ahmed.ali.radwan94@gmail.com>
2021-02-25 15:49:29 +0000

    added IMU GPS ground truth

--- scripts/autorally_gazebo_block.py
+++ scripts/autorally_gazebo_block.py
@@ -34,15 +34,15 @@ launch_path = rospack.get_path('autorally_gazebo') + '/launch/autoRallyTrackGaze
 gazebo_p = subprocess.Popen('vglrun roslaunch {} &'.format(launch_path), shell=True)
 print "starting autorally block launch file!"
 
-# pub_leftcam = rospy.Publisher('/block/left_camera/image_raw', Image, queue_size=10)
+pub_leftcam = rospy.Publisher('/block/left_camera/image_raw', Image, queue_size=10)
 pub_rightcam = rospy.Publisher('/block/right_camera/image_raw', Image, queue_size=10)
-# pub_leftcaminfo = rospy.Publisher('/block/left_camera/camera_info', CameraInfo, queue_size=10)
-# pub_rightcaminfo = rospy.Publisher('/block/right_camera/camera_info', CameraInfo, queue_size=10)
+pub_leftcaminfo = rospy.Publisher('/block/left_camera/camera_info', CameraInfo, queue_size=10)
+pub_rightcaminfo = rospy.Publisher('/block/right_camera/camera_info', CameraInfo, queue_size=10)
 pub_chassisState = rospy.Publisher('/block/chassisState', chassisState, queue_size=10)
 pub_wheelSpeeds = rospy.Publisher('/block/wheelSpeeds', wheelSpeeds, queue_size=10)
 pub_GPS = rospy.Publisher('/block/gpsRoverStatus', NavSatFix, queue_size=10)
 pub_IMU = rospy.Publisher('/block/imu/imu', Imu, queue_size=10)
-# pub_gt = rospy.Publisher('/block/ground_truth/state', Odometry, queue_size=10)
+pub_gt = rospy.Publisher('/block/ground_truth/state', Odometry, queue_size=10)
 
 pub_command = rospy.Publisher('/OCS/chassisCommand', chassisCommand, queue_size=10)
 
@@ -55,17 +55,17 @@ def rightcamCallback(data_rightcam):
     print "got right cam"
     pub_rightcam.publish(data_rightcam)
 
-# def leftcamCallback(data_leftcam):
-#     print "got left cam"
-#     pub_leftcam.publish(data_leftcam)
+def leftcamCallback(data_leftcam):
+    # print "got left cam"
+    pub_leftcam.publish(data_leftcam)
 
-# def leftcaminfoCallback(data_leftcaminfo):
-#     print "got left cam info"
-#     pub_leftcaminfo.publish(data_leftcaminfo)
+def leftcaminfoCallback(data_leftcaminfo):
+    # print "got left cam info"
+    pub_leftcaminfo.publish(data_leftcaminfo)
 
-# def rightcaminfoCallback(data_rightcaminfo):
-#     print "got right cam info"
-#     pub_rightcaminfo.publish(data_rightcaminfo)
+def rightcaminfoCallback(data_rightcaminfo):
+    # print "got right cam info"
+    pub_rightcaminfo.publish(data_rightcaminfo)
 
 def chassisStateCallback(data_chassisState):
     # print "got chassis state"
@@ -83,19 +83,19 @@ def GPSCallback(data_GPS):
     # print "got GPS"
     pub_GPS.publish(data_GPS)
 
-# def gtCallback(data_gt):
-#     # print "got ground truth"
-#     pub_gt.publish(data_gt)
+def gtCallback(data_gt):
+    # print "got ground truth"
+    pub_gt.publish(data_gt)
 
 rospy.Subscriber("/block/OCS/chassisCommand", chassisCommand, commandCallback)
 rospy.Subscriber("/right_camera/image_raw", Image, rightcamCallback)
-# rospy.Subscriber("/left_camera/image_raw", Image, leftcamCallback)
-# rospy.Subscriber("/right_camera/camera_info", CameraInfo, rightcaminfoCallback)
-# rospy.Subscriber("/left_camera/camera_info", CameraInfo, leftcaminfoCallback)
+rospy.Subscriber("/left_camera/image_raw", Image, leftcamCallback)
+rospy.Subscriber("/right_camera/camera_info", CameraInfo, rightcaminfoCallback)
+rospy.Subscriber("/left_camera/camera_info", CameraInfo, leftcaminfoCallback)
 rospy.Subscriber("/chassisState", chassisState, chassisStateCallback)
 rospy.Subscriber("/wheelSpeeds", wheelSpeeds, wheelSpeedsCallback)
 rospy.Subscriber("/gpsRoverStatus", NavSatFix, GPSCallback)
 rospy.Subscriber("/imu/imu", Imu, IMUCallback)
-# rospy.Subscriber("/ground_truth/state", Odometry, gtCallback)
+rospy.Subscriber("/ground_truth/state", Odometry, gtCallback)
 
 rospy.spin()
